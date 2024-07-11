fc033e985eea9528094f553e2b1a1b2d56893c63
Kaustubh Patil <patilkaustubh990@gmail.com>
2024-07-11 08:30:55 +0530

    chore: Update app.js to use socket.io for real-time communication

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const exp = require('constants');
 const express = require('express');
 const app = express();
 const http = require('http');
+const path = require('path');
 
 const socketio = require('socket.io');
 
@@ -8,6 +10,8 @@ const server = http.createServer(app);
 
 const io = socketio(server);
 
+app.set('view engine', 'ejs');
+app.set(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function (req, res){
     res.send("This is going to be an awesome project!");
