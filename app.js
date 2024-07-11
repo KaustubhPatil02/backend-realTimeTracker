d00f3025a3bd474e504ce9d89a5cda9eb6aee169
Kaustubh Patil <patilkaustubh990@gmail.com>
2024-07-11 20:42:58 +0530

    chore: Add real-time location updates using socket.io

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// backend code
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -13,8 +14,17 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function (socket) {
+    socket.on('send-location', function (data) {
+        io.emit('receive-location', {id: socket.id, ...data});
+        console.log(data);
+    });
     console.log("connected");
+    socket.on('disconnect', function () {
+        io.emit('remove-location', socket.id);
+    });
+    
 });
+
 
 app.get("/", function (req, res) {
     res.render("index");
